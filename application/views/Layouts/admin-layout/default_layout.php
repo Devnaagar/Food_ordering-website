3ab7f9224c3685026955e9941fa85243130eec48
Dev Naagar <devnaagar773@gmail.com>
2024-06-07 09:56:15 +0530

    somechanges

--- application/views/Layouts/admin-layout/default_layout.php
+++ application/views/Layouts/admin-layout/default_layout.php
@@ -33,7 +33,7 @@
 <div class="wrapper">
     <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="<?php echo base_url(); ?>assets/admin/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="<?php echo base_url(); ?>assets/admin/dist/img/geetalogo.png" alt="AdminLTELogo" height="60" width="60">
   </div>
     <?php $this->load->view('Layouts/admin-layout/header'); ?>
     <?php $this->load->view('Layouts/admin-layout/sidebar'); ?>
